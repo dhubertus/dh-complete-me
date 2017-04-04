70d580855e1571c0b2d2d392c44ba1be59e12bad
Dave Hubertus <davehubertus@gmail.com>
2017-04-04 15:33:58 -0600

    has working insert find and suggest

--- scripts/Tree.js
+++ scripts/Tree.js
@@ -1,32 +1,39 @@
 import Node from '../scripts/node'
+const text = "/usr/share/dict/words"
+var fs = require('fs');
+require('locus')
 
 export default class Tree {
   constructor() {
     this.rootNode = new Node(null)
+    this.count = 0;
   }
 
   insert(word) {
     let node = this.rootNode
     let splitArray = word.split('')
+    let address = ''
 
     splitArray.forEach((letter) => {
-      // let newNode = new Node(letter)
 
       if (node.children[letter]) {
+        address = address + letter
         return node = node.children[letter]
       }
 
       node.children[letter] = new Node(letter);
       node = node.children[letter];
+      address = address + letter
+      node.address = address
     })
 
     node.isWord = true
-    node.address = word
+    this.count ++;
   }
 
-  findNode(word) {
+  findNode(text) {
     let node = this.rootNode
-    let splitArray = word.split('')
+    let splitArray = text.split('')
 
     splitArray.forEach((letter) => {
 
@@ -35,9 +42,64 @@ export default class Tree {
       }
     })
 
-    if (node.address === word) {
-      console.log(node)
+    if (node.address === text) {
       return node
     }
   }
+
+  // suggest(text, suggested) {
+  //
+  //
+  //
+  //   let node = this.findNode(text)
+  //   var suggested = suggested || []
+  //
+  //   console.log(suggested)
+  //
+  //   let nextLetter =
+  //   Object.getOwnPropertyNames(node.children).toString()
+  //   console.log(nextLetter)
+  //
+  //   if(node.isWord) {
+  //     suggested.push(text)
+  //   }
+  //
+  //   if(!node.isWord) {
+  //     let newText = text + nextLetter
+  //     // console.log(newText.toString())
+  //     this.suggest(newText, suggested)
+  //   }
+  //   console.log(suggested)
+  //   return suggested
+  // }
+
+  suggest(text) {
+    let node = this.findNode(text)
+    var suggested = []
+    // eval(locus)
+    const filterKeys = (node) => {
+      // eval(locus)
+      if(node.isWord){
+        suggested.push(node.address)
+      }
+
+      Object.keys(node.children).forEach((key) => {
+        filterKeys(node.children[key])
+      })
+    }
+
+    filterKeys(node)
+    return suggested
+
+  }
+
+  populate() {
+    let dictionary = fs.readFileSync(text).toString().trim().split('\n')
+
+    dictionary.forEach((word) => {
+      this.insert(word)
+    })
+  }
+
+
 }
--- tests/tree-test.js
+++ tests/tree-test.js
@@ -23,7 +23,6 @@ describe('Tree', () => {
     tree.insert('arts')
     tree.insert('arty')
 
-
     expect(tree.rootNode.children.a.data).to.eq('a')
     expect(tree.rootNode.children.a.children.r.data).to.eq('r')
     expect(tree.rootNode.children.a.children.r.children.t.data).to.eq('t')
@@ -36,8 +35,48 @@ describe('Tree', () => {
     tree.insert('art')
     tree.insert('polymer')
     tree.findNode('polymer')
-    eval(locus)
   })
+
+  describe('Method: findNode', () => {
+    it('should return the last node of the entry by using its address', () => {
+      let tree = new Tree();
+
+      tree.insert('polymer')
+      tree.insert('arts')
+
+      expect(tree.findNode('pol').data).to.eq('l')
+      expect(tree.findNode('po').data).to.eq('o')
+    })
+  })
+
+  describe('Method: suggest', () => {
+    it('should return an array of possible', () => {
+
+      let tree = new Tree();
+
+      tree.insert('pizza')
+      tree.insert('pit')
+      tree.insert('pickle')
+      tree.insert('pices')
+      tree.insert('pinch')
+
+      let result = tree.suggest('pi')
+
+      expect(result).to.deep.eq(['pizza', 'pit', 'pickle', 'pices', 'pinch'])
+    })
+  })
+
+  describe('Dictionary population', () => {
+    it('should have 235886 words counted', () => {
+      let tree = new Tree();
+
+      tree.populate()
+
+      expect(tree.count).to.be.eq(235886)
+
+    })
+  })
+
 
 
 })
